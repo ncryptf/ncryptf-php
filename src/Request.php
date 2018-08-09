00eb2e13a6a8d7b98b13769c3049ff82de0efac4
Charles R. Portwood II <charlesportwoodii@erianna.com>
2018-08-09 15:59:39 -0500

    Adding request/response decryption classes

---
+++ src/Request.php
@@ -0,0 +1,84 @@
+<?php declare(strict_types=1);
+
+namespace ncryptf;
+
+use InvalidArgumentException;
+use SodiumException;
+
+class Request
+{
+    /**
+     * Sodium CryptoBox Keypair
+     *
+     * @var string
+     */
+    private $keypair;
+
+    /**
+     * 24 byte nonce
+     * 
+     * @var string
+     */
+    private $nonce;
+
+    /**
+     * Constructor
+     * 
+     * @param string $secretKey The 32 byte secret key
+     * @param string $publicKey The 32 byte public key
+     */
+    public function __construct(string $secretKey, string $publicKey)
+    {
+        try {
+            $this->keypair = \sodium_crypto_box_keypair_from_secretkey_and_publickey(
+                $secretKey,
+                $publicKey
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+    }
+
+    /**
+     * Encrypts a request
+     * 
+     * @param string $request   The raw HTTP request as a string
+     * @param string $nonce     Optional nonce. If not provided, a 24 byte nonce will be generated
+     * @return string
+     */
+    public function encrypt(string $request, string $nonce = null)
+    {
+        if ($nonce === null) {
+            $nonce = \random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES);
+        }
+
+        try {
+            return \sodium_crypto_box(
+                $request,
+                $nonce,
+                $this->keypair
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+    }
+
+    /**
+     * Creates a detached signature for the keypair
+     * 
+     * @param string $request
+     * @param string $secretKey
+     * @return string
+     */
+    public function sign(string $request, string $secretKey)
+    {
+        try {
+            return \sodium_crypto_sign_detached(
+                $request,
+                $secretKey
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+    }
+}
---
+++ src/Response.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace ncryptf;
+
+use InvalidArgumentException;
+use SodiumException;
+
+class Response
+{
+    /**
+     * Sodium CryptoBox Keypair
+     *
+     * @var string
+     */
+    private $keypair;
+
+    /**
+     * Constructor
+     * 
+     * @param string $secretKey The 32 byte secret key
+     * @param string $publicKey The 32 byte public key
+     */
+    public function __construct(string $secretKey, string $publicKey)
+    {
+        try {
+            $this->keypair = \sodium_crypto_box_keypair_from_secretkey_and_publickey(
+                $secretKey,
+                $publicKey
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+    }
+
+    /**
+     * Decrypts a given response with a nonce
+     * This will return the decrypted string of decrypt was successful, and false otherwise
+     * 
+     * @param string $response  The encrypted HTTP response, as a multi-byte string
+     * @param string $nonce     The 32 byte nonce
+     * @return string|bool
+     */
+    public function decrypt(string $response, string $nonce)
+    {
+        try {
+            return \sodium_crypto_box_open(
+                $response,
+                $nonce,
+                $this->keypair
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArguementException($e->getMessage());
+        }
+    }
+
+    /**
+     * Returns true if the signature validates the response
+     * 
+     * @param string $response  The raw http response, after decoding
+     * @param string $signature The raw multi-byte signature
+     * @param string $publicKey The signing public key
+     * @return bool
+     */
+    public function isSignatureValid(string $response, string $signature, string $publicKey)
+    {
+        try {
+            return \sodium_crypto_sign_verify_detached(
+                $signature,
+                $response,
+                $publicKey
+            );
+        } catch (SodiumException $e) {
+            throw new InvalidArguementException($e->getMessage());
+        }
+    }
+}
