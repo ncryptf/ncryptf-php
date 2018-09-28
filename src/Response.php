d6e32678b9cd2282b64e96ba935b5128e7e2f0cf
Charles R. Portwood II <charlesportwoodii@erianna.com>
2018-09-28 16:59:02 -0500

    Adding new API's to extract the public key and signature key easily

--- src/Response.php
+++ src/Response.php
@@ -59,7 +59,7 @@ class Response
      */
     public function decrypt(string $response, string $nonce = null) : string
     {
-        $version = $this->getVersion($response);
+        $version = static::getVersion($response);
         if ($version === 2) {
             if (\strlen($response) < 236) {
                 throw new DecryptionFailedException;
@@ -126,11 +126,15 @@ class Response
                 throw new InvalidArgumentException('Keypair not available');
             }
 
-            return \sodium_crypto_box_open(
+            if ($result = \sodium_crypto_box_open(
                 $response,
                 $nonce,
                 $this->keypair->getSodiumKeypair()
-            );
+            )) {
+                return $result;
+            }
+            
+            throw new DecryptionFailedException;
         } catch (SodiumException $e) {
             throw new InvalidArgumentException($e->getMessage());
         }
@@ -160,12 +164,53 @@ class Response
     }
 
     /**
+     * Extracts the public key from a v2 response
+     *
+     * @param string $response
+     * @return string
+     */
+    public static function getPublicKeyFromResponse(string $response) : string
+    {
+        $version = static::getVersion($response);
+        if ($version === 2) {
+            if (\strlen($response) < 236) {
+                throw new InvalidArgumentException;
+            }
+            
+            return \substr($response, 28, 32);
+        }
+
+        throw new InvalidArgumentException('The response provided is not suitable for public key extraction.');
+    }
+
+    /**
+     * Extracts the signature public key from a v2 response
+     *
+     * @param string $response
+     * @return string
+     */
+    public static function getSignaturePublicKeyFromResponse(string $response) : string
+    {
+        $version = static::getVersion($response);
+        if ($version === 2) {
+            if (\strlen($response) < 236) {
+                throw new InvalidArgumentException;
+            }
+            
+            $payload = \substr($response, 0, \strlen($response) - 64);
+            return  \substr($payload, -32);
+        }
+
+        throw new InvalidArgumentException('The response provided is not suitable for public key extraction.');
+    }
+
+    /**
      * Extracts the version from the response
      *
      * @param string $response  The encrypted http response
      * @return int
      */
-    private function getVersion(string $response) : int
+    public static function getVersion(string $response) : int
     {
         if (\strlen($response) < 16) {
             throw new DecryptionFailedException("Message length is too short to determine version.");
